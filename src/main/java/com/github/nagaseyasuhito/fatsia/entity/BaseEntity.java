fab00940f34e86350fd60a30b483e04a427582ad
nagaseyasuhito <nagase@nagaseyasuhito.net>
2012-01-19 17:14:36 +0900

    add toString method

--- src/main/java/com/github/nagaseyasuhito/fatsia/entity/BaseEntity.java
+++ src/main/java/com/github/nagaseyasuhito/fatsia/entity/BaseEntity.java
@@ -1,45 +1,71 @@
 package com.github.nagaseyasuhito.fatsia.entity;
 
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.MappedSuperclass;
 
 import org.apache.commons.lang3.builder.CompareToBuilder;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
 
+import com.google.common.collect.Lists;
+
+@SuppressWarnings("serial")
 @MappedSuperclass
-@TypeDefs({ @TypeDef(typeClass = String.class, defaultForType = CharSequence.class), @TypeDef(typeClass = Long.class, defaultForType = Number.class) })
+@TypeDefs({@TypeDef(typeClass = String.class, defaultForType = CharSequence.class), @TypeDef(typeClass = Long.class, defaultForType = Number.class) })
 public abstract class BaseEntity<T extends Serializable> implements Serializable, Comparable<BaseEntity<T>> {
-	private static final long serialVersionUID = -1908110108935988177L;
 
-	public abstract T getId();
+    public abstract T getId();
 
-	@Override
-	public int compareTo(BaseEntity<T> o) {
-		return new CompareToBuilder().append(this.getId(), o.getId()).toComparison();
-	}
+    @Override
+    public int compareTo(BaseEntity<T> o) {
+        return new CompareToBuilder().append(this.getId(), o.getId()).toComparison();
+    }
 
-	@Override
-	public int hashCode() {
-		return this.getId() == null ? super.hashCode() : this.getId().hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return this.getId() == null ? super.hashCode() : this.getId().hashCode();
+    }
 
-	@Override
-	public boolean equals(Object object) {
-		if (object == null) {
-			return false;
-		}
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
 
-		if (!this.getClass().isAssignableFrom(object.getClass()) && !object.getClass().isAssignableFrom(this.getClass())) {
-			return false;
-		}
+        if (!this.getClass().isAssignableFrom(object.getClass()) && !object.getClass().isAssignableFrom(this.getClass())) {
+            return false;
+        }
 
-		BaseEntity<?> entity = (BaseEntity<?>) object;
-		if (this.getId() == null || entity.getId() == null) {
-			return this.equals(entity);
-		}
+        BaseEntity<?> entity = (BaseEntity<?>) object;
+        if (this.getId() == null || entity.getId() == null) {
+            return this.equals(entity);
+        }
 
-		return this.getId().equals(entity.getId());
-	}
+        return this.getId().equals(entity.getId());
+    }
+
+    @Override
+    public String toString() {
+        Class<?> clazz = this.getClass();
+        List<String> excludeFieldNames = Lists.newArrayList();
+        do {
+            for (Field field : clazz.getDeclaredFields()) {
+                Class<?> type = field.getType();
+
+                if (Collection.class.isAssignableFrom(type) || Map.class.isAssignableFrom(type)) {
+                    excludeFieldNames.add(field.getName());
+                }
+            }
+        } while (null != (clazz = clazz.getSuperclass()));
+
+        ReflectionToStringBuilder reflectionToStringBuilder = new ReflectionToStringBuilder(this);
+        reflectionToStringBuilder.setExcludeFieldNames(excludeFieldNames.toArray(new String[0]));
+        return reflectionToStringBuilder.toString();
+    }
 }
