6e816c794573161a5b91dc73e2242375136bd9a4
nagaseyasuhito <nagase@nagaseyasuhito.net>
2012-02-02 10:27:31 +0900

    refactored

--- src/main/java/com/github/nagaseyasuhito/fatsia/FatsiaAnnotationProcessor.java
+++ src/main/java/com/github/nagaseyasuhito/fatsia/FatsiaAnnotationProcessor.java
@@ -10,14 +10,13 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.persistence.Entity;
 import javax.tools.JavaFileObject;
 
 import com.github.nagaseyasuhito.fatsia.criteria.ConditionalCriteria;
 import com.github.nagaseyasuhito.fatsia.criteria.Criteria;
-import com.google.common.base.Function;
-import com.google.common.collect.Collections2;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_6)
 @SupportedAnnotationTypes("javax.persistence.Entity")
@@ -28,20 +27,8 @@ public class FatsiaAnnotationProcessor extends AbstractProcessor {
         Set<? extends Element> elements = environment.getElementsAnnotatedWith(Entity.class);
 
         try {
-            for (Class<?> clazz : Collections2.transform(elements, new Function<Element, Class<?>>() {
-                @Override
-                public Class<?> apply(Element input) {
-                    try {
-                        return Class.forName(((TypeElement) input).getQualifiedName().toString());
-                    } catch (ClassNotFoundException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            })) {
-                CharSequence packageName = clazz.getPackage().getName();
-                CharSequence className = clazz.getSimpleName();
-
-                this.buildClass(clazz, packageName, className, ConditionalCriteria.values());
+            for (Element element : elements) {
+                this.buildClass((TypeElement) element, ConditionalCriteria.values());
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -49,12 +36,16 @@ public class FatsiaAnnotationProcessor extends AbstractProcessor {
         return true;
     }
 
-    public void buildClass(Class<?> clazz, CharSequence packageName, CharSequence className, Criteria[] criterias) throws IOException {
+    public void buildClass(TypeElement element, Criteria[] criterias) throws IOException {
+        CharSequence fqcn = element.getQualifiedName();
+        CharSequence className = element.getSimpleName();
+        CharSequence packageName = ((PackageElement) element.getEnclosingElement()).getQualifiedName();
+
         StringBuffer buffer = new StringBuffer();
         buffer.append("package " + packageName + ";");
         buffer.append("public class " + className + "Criteria {");
         for (Criteria criteria : criterias) {
-            buffer.append(this.buildSource(criteria, clazz));
+            buffer.append(this.buildSource(criteria, element));
         }
         buffer.append("}");
 
@@ -64,8 +55,8 @@ public class FatsiaAnnotationProcessor extends AbstractProcessor {
         javaWriter.close();
     }
 
-    public CharSequence buildSource(Criteria criteria, Class<?> clazz) throws IOException {
-        CharSequence fqcn = clazz.getCanonicalName();
+    public CharSequence buildSource(Criteria criteria, TypeElement element) throws IOException {
+        CharSequence fqcn = element.getQualifiedName();
 
         StringBuffer buffer = new StringBuffer();
         buffer.append("public static class " + criteria.getCriteriaClass().getSimpleName());
