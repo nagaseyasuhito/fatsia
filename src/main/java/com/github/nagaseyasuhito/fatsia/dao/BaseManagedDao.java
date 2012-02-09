9210850f6e497916225cf1e382226bca54727230
nagaseyasuhito <nagase@nagaseyasuhito.net>
2012-02-09 22:52:39 +0900

    remove DAO mechanism

--- src/main/java/com/github/nagaseyasuhito/fatsia/dao/BaseManagedDao.java
+++
@@ -1,6 +0,0 @@
-package com.github.nagaseyasuhito.fatsia.dao;
-
-import com.github.nagaseyasuhito.fatsia.entity.BaseManagedEntity;
-
-public abstract class BaseManagedDao<T extends BaseManagedEntity> extends BaseDao<Long, T> {
-}
