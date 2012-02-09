fabf56d33869c2daf1ebfa52b3fbc68ea6539206
nagaseyasuhito <nagase@nagaseyasuhito.net>
2012-02-09 17:40:38 +0900

    modify In criteria mechanism

--- src/main/java/com/github/nagaseyasuhito/fatsia/Criterias.java
+++ src/main/java/com/github/nagaseyasuhito/fatsia/Criterias.java
@@ -11,50 +11,51 @@ import com.github.nagaseyasuhito.fatsia.criteria.LesserThan;
 import com.github.nagaseyasuhito.fatsia.criteria.Like;
 import com.github.nagaseyasuhito.fatsia.criteria.Null;
 import com.github.nagaseyasuhito.fatsia.criteria.Or;
+import com.github.nagaseyasuhito.fatsia.entity.BaseEntity;
 
 public class Criterias {
 
-	public static <T> And<T> and() {
-		return new And<T>();
-	}
+    public static <T> And<T> and() {
+        return new And<T>();
+    }
 
-	public static <T> In<T> in() {
-		return new In<T>();
-	}
+    public static <T extends BaseEntity<?>> In<T> in() {
+        return new In<T>();
+    }
 
-	public static <T> Or<T> or() {
-		return new Or<T>();
-	}
+    public static <T> Or<T> or() {
+        return new Or<T>();
+    }
 
-	public static <T> Null<T> nullable() {
-		return new Null<T>();
-	}
+    public static <T> Null<T> nullable() {
+        return new Null<T>();
+    }
 
-	public static <T extends CharSequence & Comparable<T>> Like<T> like(T value) {
-		return new Like<T>(value);
-	}
+    public static <T extends CharSequence & Comparable<T>> Like<T> like(T value) {
+        return new Like<T>(value);
+    }
 
-	public static <T extends Comparable<T>> Between<T> between(T from, T to) {
-		return new Between<T>(from, to);
-	}
+    public static <T extends Comparable<T>> Between<T> between(T from, T to) {
+        return new Between<T>(from, to);
+    }
 
-	public static <T extends Comparable<T>> Equal<T> eq(T value) {
-		return new Equal<T>(value);
-	}
+    public static <T extends Comparable<T>> Equal<T> eq(T value) {
+        return new Equal<T>(value);
+    }
 
-	public static <T extends Comparable<T>> GreaterEqual<T> ge(T value) {
-		return new GreaterEqual<T>(value);
-	}
+    public static <T extends Comparable<T>> GreaterEqual<T> ge(T value) {
+        return new GreaterEqual<T>(value);
+    }
 
-	public static <T extends Comparable<T>> GreaterThan<T> gt(T value) {
-		return new GreaterThan<T>(value);
-	}
+    public static <T extends Comparable<T>> GreaterThan<T> gt(T value) {
+        return new GreaterThan<T>(value);
+    }
 
-	public static <T extends Comparable<T>> LesserEqual<T> le(T value) {
-		return new LesserEqual<T>(value);
-	}
+    public static <T extends Comparable<T>> LesserEqual<T> le(T value) {
+        return new LesserEqual<T>(value);
+    }
 
-	public static <T extends Comparable<T>> LesserThan<T> lt(T value) {
-		return new LesserThan<T>(value);
-	}
+    public static <T extends Comparable<T>> LesserThan<T> lt(T value) {
+        return new LesserThan<T>(value);
+    }
 }
